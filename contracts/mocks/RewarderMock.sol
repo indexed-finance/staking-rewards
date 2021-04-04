aeeb3ee714555cb9c542096c6262e3fcad288fe9
d1ll0n <dillonkellar@protonmail.com>
2021-04-03 20:10:49 -0500

    Replaced 4 space indentation with 2

--- contracts/mocks/RewarderMock.sol
+++ contracts/mocks/RewarderMock.sol
@@ -7,30 +7,30 @@ import "@boringcrypto/boring-solidity/contracts/libraries/BoringMath.sol";
 
 
 contract RewarderMock is IRewarder {
-    using BoringMath for uint256;
-    using BoringERC20 for IERC20;
-    uint256 private immutable rewardMultiplier;
-    IERC20 private immutable rewardToken;
-    uint256 private constant REWARD_TOKEN_DIVISOR = 1e18;
+  using BoringMath for uint256;
+  using BoringERC20 for IERC20;
+  uint256 private immutable rewardMultiplier;
+  IERC20 private immutable rewardToken;
+  uint256 private constant REWARD_TOKEN_DIVISOR = 1e18;
 
-    constructor (uint256 _rewardMultiplier, IERC20 _rewardToken) public {
-        rewardMultiplier = _rewardMultiplier;
-        rewardToken = _rewardToken;
-    }
+  constructor (uint256 _rewardMultiplier, IERC20 _rewardToken) public {
+    rewardMultiplier = _rewardMultiplier;
+    rewardToken = _rewardToken;
+  }
 
-    function onStakingReward(uint256, address user, uint256 rewardAmount) override external {
-        uint256 pendingReward = rewardAmount.mul(rewardMultiplier) / REWARD_TOKEN_DIVISOR;
-        uint256 rewardBal = rewardToken.balanceOf(address(this));
-        if (pendingReward > rewardBal) {
-            rewardToken.safeTransfer(user, rewardBal);
-        } else {
-            rewardToken.safeTransfer(user, pendingReward);
-        }
+  function onStakingReward(uint256, address user, uint256 rewardAmount) override external {
+    uint256 pendingReward = rewardAmount.mul(rewardMultiplier) / REWARD_TOKEN_DIVISOR;
+    uint256 rewardBal = rewardToken.balanceOf(address(this));
+    if (pendingReward > rewardBal) {
+      rewardToken.safeTransfer(user, rewardBal);
+    } else {
+      rewardToken.safeTransfer(user, pendingReward);
     }
-    function pendingTokens(uint256 pid, address user, uint256 rewardAmount) override external returns (address[] memory rewardTokens, uint256[] memory rewardAmounts) {
-        rewardTokens = new address[](1);
-        rewardTokens[0] = address(rewardToken);
-        rewardAmounts = new uint256[](1);
-        rewardAmounts[0] = rewardAmount.mul(rewardMultiplier) / REWARD_TOKEN_DIVISOR;
-    }
+  }
+  function pendingTokens(uint256 pid, address user, uint256 rewardAmount) override external returns (address[] memory rewardTokens, uint256[] memory rewardAmounts) {
+    rewardTokens = new address[](1);
+    rewardTokens[0] = address(rewardToken);
+    rewardAmounts = new uint256[](1);
+    rewardAmounts[0] = rewardAmount.mul(rewardMultiplier) / REWARD_TOKEN_DIVISOR;
+  }
 }
